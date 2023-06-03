71164f2fe52b70f5d80b94d733edff3625a527ec
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-06-03 23:53:14 +0300

    Add post and user component 1111

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                Profile
+            </div>
+            <div>
+                Messages
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Music
+            </div>
+            <div>
+                Settings
+            </div>
+            <div>
+                Users
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.bestattravel.co.uk/the-edit/wp-content/uploads/2021/02/New-York-Landmarks.jpg"
+                     alt="main"/>
+            </div>
+
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                My post
+            </div>
+
+            <div>
+                New post
+            </div>
+
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
