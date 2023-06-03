689d1469c61e6e13525b99fdd1e78c2dc2ca8dca
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-06-03 23:15:36 +0300

    Add post and user component too

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <header className='header'>
+            <img src="https://cdn.worldvectorlogo.com/logos/facebook-messenger-3.svg"
+                 alt="logo"/>
+        </header>
+    );
+};
+
+export default Post;
