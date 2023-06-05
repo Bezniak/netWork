8072e80e098af4d741bfa6e70a6680ff9b8a646d
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-06-05 20:42:08 +0300

    Revert "dev commit 22"
    This reverts commit 45f7dc921bd29da14ad20e3ec11e1b56f523b5c4.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,11 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div className={s.postsBlock}>
-            <h3>My post</h3>
+        <div>
+            My post
             <div>
-                <div>
-                    <textarea placeholder='Say something'></textarea>
-                </div>
-                <div>
-                    <button>Add post</button>
-                </div>
+                <textarea placeholder='Say something'></textarea>
+                <button>Add post</button>
             </div>
             <div className={s.posts}>
                 <Post message='Hi, how are you?' likesCount='16'/>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 
 const Profile = () => {
     return (
         <div className={s.content}>
-            <ProfileInfo/>
+            <div>
+                <img src="https://www.bestattravel.co.uk/the-edit/wp-content/uploads/2021/02/New-York-Landmarks.jpg"
+                     alt="main"/>
+            </div>
+            <div>
+                ava + description
+            </div>
             <MyPosts/>
         </div>
     );
