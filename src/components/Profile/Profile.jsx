45f7dc921bd29da14ad20e3ec11e1b56f523b5c4
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-06-05 20:40:34 +0300

    dev commit 22

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 
 const Profile = () => {
     return (
         <div className={s.content}>
-            <div>
-                <img src="https://www.bestattravel.co.uk/the-edit/wp-content/uploads/2021/02/New-York-Landmarks.jpg"
-                     alt="main"/>
-            </div>
-            <div>
-                ava + description
-            </div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     );
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = (props) => {
+    return (
+        <div>
+            <div>
+                <img src="https://www.bestattravel.co.uk/the-edit/wp-content/uploads/2021/02/New-York-Landmarks.jpg"
+                     alt="main"/>
+            </div>
+            <div className={s.descriptionBlock}>
+                ava + description
+            </div>
+        </div>
+    );
+};
+
+export default ProfileInfo;
