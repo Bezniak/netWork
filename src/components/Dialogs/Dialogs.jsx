4ba5e20830f8a21a86224122df1d9a54264f4052
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-06-23 16:19:54 +0300

    68 Navigate

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {Navigate} from "react-router-dom";
 
 
 const Dialogs = (props) => {
@@ -22,6 +23,11 @@ const Dialogs = (props) => {
     let onNewMessageChange = (e) => {
         let body = e.target.value;
         props.updateNewMessageBody(body)
+    }
+
+
+    if (!props.isAuth) {
+        return <Navigate to={"/login"}/>
     }
 
 
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css';
 import Profile from "./Profile";
 import {connect} from "react-redux";
 import {getUserProfile} from "../../redux/profile-reducer";
-import {useLocation, useNavigate, useParams} from "react-router-dom";
+import {Navigate, useLocation, useNavigate, useParams} from "react-router-dom";
 
 function withRouter(Component) {
     function ComponentWithRouterProp(props) {
@@ -34,6 +34,11 @@ class ProfileContainer extends React.Component {
     }
 
     render() {
+
+        if (!this.props.isAuth) {
+            return <Navigate to={'/login'}/>
+        }
+
         return (
             <div className={s.content}>
                 <Profile {...this.props} profile={this.props.profile}/>
@@ -44,6 +49,7 @@ class ProfileContainer extends React.Component {
 
 const mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
+    isAuth: state.auth.isAuth,
 })
 
 
