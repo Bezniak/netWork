ca3b6bfef4d23298abe3e616dd95b53498d9e670
Ivan Bezniak <ivan.bezniak@gmail.com>
2023-06-29 14:09:39 +0300

    75 login form with Formik

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,68 @@
-import React from 'react';
-import s from './Login.module.css';
+import {useFormik} from 'formik';
+import * as yup from "yup";
+import s from './Login.module.css'
 
-const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+const onSubmit = async (values, actions) => {
+    actions.resetForm();
 };
 
+const basicSchema = yup.object().shape({
+    email: yup.string().email("Please enter a valid email").required("Required"),
+    password: yup.string().required("Required"),
+});
+
+const Login = () => {
+    const {
+        values,
+        errors,
+        touched,
+        isSubmitting,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+    } = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            toggle: false,
+            checked: [],
+        },
+        validationSchema: basicSchema,
+        onSubmit,
+    });
+
+    return (
+        <form className={s.form} onSubmit={handleSubmit} autoComplete="off">
+            <label htmlFor="email">Email</label>
+            <input
+                value={values.email}
+                onChange={handleChange}
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                onBlur={handleBlur}
+                className={errors.email && touched.email ? s.inputError : ""}
+            />
+            {errors.email && touched.email && <p className="error">{errors.email}</p>}
+
+            <label htmlFor="password">Password</label>
+            <input
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.password && touched.password ? s.inputError : ""}
+            />
+            {errors.password && touched.password && (
+                <p className="error">{errors.password}</p>
+            )}
+
+            <button disabled={isSubmitting} type="submit">
+                Submit
+            </button>
+        </form>
+    );
+};
 export default Login;
